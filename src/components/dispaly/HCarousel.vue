777316388c7af1879b0cec7014ef2daca76bed88
Hell Vision <2856782165@qq.com>
2022-02-20 02:17:04 +0800

    【功能新增】增加swiper插件|增加博客轮播图|博客页面基本布局

---
+++ src/components/dispaly/HCarousel.vue
@@ -0,0 +1,139 @@
+<template>
+    <div id="h-carousel">
+        <div class="swiper-box">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide" v-for="(item,index) in images" :key="index">
+                        <a :href="item.href || item.src || item" :target="item.target || '_self'">
+                            <img class="swiper-img" :src="item.src || item" alt=""/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Swiper from "swiper/swiper-bundle"
+import "swiper/swiper-bundle.css"
+
+export default {
+    name: "HCarousel",
+    props: {
+        images: {
+            type: Array,
+            default: () => {
+                return [
+                    {src: '/static/config/blogCarousel/白鹭归庭.png', href: 'https://www.bilibili.com/video/BV1io4y1X7HU', target: "_blank"},
+                    {src: '/static/config/blogCarousel/神女劈观.png', href: 'https://www.bilibili.com/video/BV1kS4y1T7kK', target: "_blank"},
+                    {src: '/static/config/blogCarousel/真夏飞焰.png', href: 'https://www.bilibili.com/video/BV1Ro4y1S7f1', target: "_blank"},
+                ]
+            }
+        },
+        option: {
+            type: Object,
+            default: () => {
+                return {}
+            }
+        }
+    },
+    data() {
+        return {
+            default_option: {
+                effect: 'coverflow',
+                loop: true,
+                grabCursor: true,
+                centeredSlides: true,
+                slidesPerView: 'auto',
+                slideToClickedSlide: true,
+                autoplay: {
+                    delay: 2000,
+                    disableOnInteraction: false,
+                },
+                coverflow: {
+                    rotate: 10,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: false,
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                spaceBetween: 30,
+            },
+            swiper: null,
+        }
+    },
+    mounted() {
+        this.swiper = new Swiper('.swiper-container', Object.assign(this.default_option, this.option))
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang="less">
+
+#h-carousel {
+    width: 100%;
+
+    .swiper-box {
+        width: 100%;
+        margin: 0 auto;
+        overflow: hidden;
+        .swiper-container {
+            text-align: center;
+            z-index: 0;
+            width: 70%;
+            margin: 0 auto;
+            padding-top: 10px;
+            padding-bottom: 15px;
+
+
+            .swiper-wrapper {
+
+                .swiper-slide {
+                    background-position: center;
+                    background-size: cover;
+                    width: 80%;
+
+                    .swiper-img {
+                        border-radius: 10px;
+                        width: 100%;
+                    }
+
+
+                }
+            }
+
+        }
+    }
+
+    .swiper-slide-prev {
+        transform: translateX(60%) scale(80%) !important;
+
+        * {
+            pointer-events: none;
+        }
+    }
+
+    .swiper-slide-active {
+        z-index: 10 !important;
+        transform: translateX(0) scale(100%) !important;
+    }
+
+    .swiper-slide-next {
+        transform: translateX(-60%) scale(80%) !important;
+
+        * {
+            pointer-events: none;
+        }
+    }
+}
+
+
+</style>
--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -5,21 +5,101 @@
 
 <template>
     <div id="blog-app-blog">
-        <div style="height: 400px; background: #0fbcf9"></div>
-        <a-row :gutter="24">
+        <a-row :gutter="24" style="height: 300px">
             <a-col :span="16">
-                <div style="height: 4000px; background: #34e7e4"></div>
+                <h-carousel :images="imgList" style="width: 780px; height: 270px; margin: 0 auto"/>
             </a-col>
             <a-col :span="8">
-                <a-affix :offset-top="-400">
-                    <div style="height: 800px; background: #5352ed"></div>
-                </a-affix>
+                <div style="background: #722ed1; height: 300px;">
+<!--                    -->
+                </div>
+            </a-col>
+        </a-row>
+        <a-row :gutter="24" style="margin-top: 24px;">
+            <a-col :span="16">
+                <!-- 最近更新 -->
+                <a-card class="recent-updates" :title="$t('recentUpdates')">
+                    <template #extra>
+                        <a href="#">{{ $t('allArticle') }}</a>
+                    </template>
+                    <a-list item-layout="horizontal" :data-source="projectList">
+                        <template #renderItem="{ item }">
+                            <a-list-item>
+                                <a-list-item-meta>
+                                    <template #title>
+                                        <a :href="item.href" v-title>{{ item.title }}</a>
+                                    </template>
+                                    <template #avatar>
+                                        <a-avatar :src="item.avatar" />
+                                    </template>
+                                    <template #description>
+                                        <a-typography-paragraph
+                                            :ellipsis="{ rows: 2, expandable: false}"
+                                            :content="item.description"
+                                            v-title
+                                        />
+                                        <div style="display: flex">
+                                            <span style="flex-grow: 1">2020-2-12 21:24:24</span>
+                                            <span><eye-outlined /> 111</span>
+                                        </div>
+                                    </template>
+                                </a-list-item-meta>
+                            </a-list-item>
+                        </template>
+                    </a-list>
+                </a-card>
+            </a-col>
+            <a-col :span="8">
+                <a-card :title="$t('recommend')">
+                    <a-list item-layout="horizontal" :data-source="projectList">
+                        <template #renderItem="{ item }">
+                            <a-list-item>
+                                <a-list-item-meta>
+                                    <template #title>
+                                        <a :href="item.href" v-title>{{ item.title }}</a>
+                                    </template>
+                                    <template #avatar>
+                                        <a-avatar :src="item.avatar" />
+                                    </template>
+                                </a-list-item-meta>
+                            </a-list-item>
+                        </template>
+                    </a-list>
+                </a-card>
             </a-col>
         </a-row>
     </div>
 </template>
 
 <script>
+import { EyeOutlined } from '@ant-design/icons-vue'
+import HCarousel from "@/components/dispaly/HCarousel"
+export default {
+    components: {
+        EyeOutlined,
+        HCarousel,
+    },
+    data() {
+        return {
+            imgList: [
+                {src: '/static/config/blogCarousel/白鹭归庭.png', href: 'https://www.bilibili.com/video/BV1io4y1X7HU', target: "_blank"},
+                {src: '/static/config/blogCarousel/神女劈观.png', href: 'https://www.bilibili.com/video/BV1kS4y1T7kK', target: "_blank"},
+                {src: '/static/config/blogCarousel/真夏飞焰.png', href: 'https://www.bilibili.com/video/BV1Ro4y1S7f1', target: "_blank"},
+            ],
+            projectList: [],
+        }
+    },
+    mounted() {
+        // 获取轮播列表（此处暂时由前端写死，更新时直接更新config）
+        this.$axios.get('/static/config/blogCarousel.json').then(res => {
+            this.imgList = res.data
+        })
+        // 获取进行中的项目（此处暂时由前端写死，更新时直接更新config）
+        this.$axios.get('/static/config/projectList.json').then(res => {
+            this.projectList = res.data
+        })
+    }
+}
 </script>
 
 <style lang="less">
