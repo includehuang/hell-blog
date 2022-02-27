a8128dcbd07d19558ab337efc85caae07084c314
Hell Vision <2856782165@qq.com>
2022-02-27 19:05:17 +0800

    【功能新增】i18n覆盖问题修复

--- src/lang/i18n.js
+++ src/lang/i18n.js
@@ -1,10 +1,25 @@
 import { createI18n } from "vue-i18n"
 import i18nMessageCreate from "@/assets/js/i18nCreate"
 import componentsMessages from "@/components/lang/i18n"
-const messages = i18nMessageCreate('lang/message', require.context('@/lang/message', true, /\.js$/))
-Object.assign(messages, componentsMessages)
+const baseMessages = i18nMessageCreate('lang/message', require.context('@/lang/message', true, /\.js$/))
+const messageList = []
+const langList = {}
+messageList.push(baseMessages)
+messageList.push(componentsMessages)
+function i18nMap(key, value) {
+    if (langList[key]) {
+        langList[key] = {...langList[key], ...value}
+    }else {
+        langList[key] = value
+    }
+}
+messageList.forEach(item => {
+    for (const key in item) {
+        i18nMap(key, item[key])
+    }
+})
 export default createI18n({
     locale: 'zh-cn', // set locale
     fallbackLocale: 'zh-cn', // set fallback locale
-    messages, // set locale messages
+    messages: langList, // set locale messages
 })
